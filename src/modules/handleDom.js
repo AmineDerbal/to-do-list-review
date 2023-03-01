beaea6eedfd17193bcbef8a3f309bab8f269edb3
AmineD931 <aminedoran31@gmail.com>
2023-03-01 14:58:35 +0100

    fix variable calls

--- src/modules/handleDom.js
+++ src/modules/handleDom.js
@@ -1,6 +1,7 @@
 const todoList = document.getElementById('todo-list-content');
 
 export const appendTodoElement = (task, todo) => {
+  const todoList = document.getElementById('todo-list-content');
   const todoElement = document.createElement('li');
   todoElement.className = 'todo-item';
   todoElement.draggable = true;
@@ -42,6 +43,7 @@ const deleteClearButton = () => {
 };
 
 export const clearTodoList = () => {
+  const todoList = document.getElementById('todo-list-content');
   todoList.innerHTML = '';
   deleteClearButton();
 };
